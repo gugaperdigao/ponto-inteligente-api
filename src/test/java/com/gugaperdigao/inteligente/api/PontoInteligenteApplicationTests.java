658d2e12f1cc2f780a1120e9bfce9f514b95f13f
Gustavo <gustavo.perdigao@castorweb.com.br>
2021-04-02 07:56:47 -0300

    Adicionado suporte ao Hikari para gerenciamento de pool de conexoes

--- src/test/java/com/gugaperdigao/inteligente/api/PontoInteligenteApplicationTests.java
+++ src/test/java/com/gugaperdigao/inteligente/api/PontoInteligenteApplicationTests.java
@@ -2,12 +2,19 @@ package com.gugaperdigao.inteligente.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PontoInteligenteApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		
+		
 	}
 
 }
